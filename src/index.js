b4fee517327072f65430f022fe4ac728e27cdff0
Naik-Aditya <aditya.naik@harivara.com>
2019-05-02 11:35:20 +0530

    today

--- src/index.js
+++ src/index.js
@@ -3,15 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import {createStore} from 'redux';
+
+
+import {
+    Provider
+} from 'react-redux';
+import {
+    createStore,
+    applyMiddleware
+} from 'redux';
 import reducer from './Contain/Store/Reducer';
 
-const store = createStore(reducer);
+const logAction = store => {
+    return next => {
+        return action => {
+            const result = next(action);
+            console.log(`Caught in the middleware ${JSON.stringify(result)}`);
+            return result;
+        }
+       
+        
+        
+    }
+}
 
-ReactDOM.render(<Provider store={store}><App /></Provider> , document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(logAction));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render( <Provider store = {
+            store
+        } > < App / > </Provider> , document.getElementById('root'));
+
+        // If you want your app to work offline and load faster, you can change
+        // unregister() to register() below. Note this comes with some pitfalls.
+        // Learn more about service workers: https://bit.ly/CRA-PWA
+        serviceWorker.unregister();
